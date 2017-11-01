aeece5d07ba266fb81ef4aa2174e2372b0e70a3e
rexbu <rex@labjk.com>
2017-11-01 18:13:51 +0800

    aaa

--- model/README.md
+++
@@ -1,4 +0,0 @@
-# model
-一个包含异步http框架、websocket、可靠udp传输的框架库
-
-依赖 rexbu/bs rexbu/basic
--- setup.sh
+++ setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-action=$2
-path=$1
+action=$1
+path=$2
 
 if [ "$action" = "init" ];then
-    git remote add $path/bs git@github.com:rexbu/bs.git
-    git remote add $path/basic git@github.com:rexbu/basic.git
-    git remote add $path/libzip git@github.com:rexbu/libzip.git
-    git remote add $path/model git@github.com:rexbu/model.git
+    git remote add bs git@github.com:rexbu/bs.git
+    git remote add basic git@github.com:rexbu/basic.git
+    git remote add libzip git@github.com:rexbu/libzip.git
+    git remote add model git@github.com:rexbu/model.git
 
-    git subtree add --prefix $path/bs bs master
-    git subtree add --prefix $path/basic basic master
-    git subtree add --prefix $path/model model master
-    git subtree add --prefix $path/3rdparty/libzip libzip master
+    git subtree add --prefix ${path}bs bs master
+    git subtree add --prefix ${path}basic basic master
+    git subtree add --prefix ${path}model model master
+    git subtree add --prefix ${path}3rdparty/libzip libzip master
 elif [ "$action" = "push" ];then
-    git subtree push --prefix $path/bs git@github.com:rexbu/bs.git master
-    git subtree push --prefix $path/basic git@github.com:rexbu/basic.git master
-    git subtree push --prefix $path/3rdparty/libzip git@github.com:rexbu/libzip.git  master
-    git subtree push --prefix $path/model git@github.com:rexbu/model.git master
+    git subtree push --prefix ${path}bs git@github.com:rexbu/bs.git master
+    git subtree push --prefix ${path}basic git@github.com:rexbu/basic.git master
+    git subtree push --prefix ${path}3rdparty/libzip git@github.com:rexbu/libzip.git  master
+    git subtree push --prefix ${path}model git@github.com:rexbu/model.git master
 elif [ "$action" = "pull" ];then
-    git subtree pull --prefix $path/bs git@github.com:rexbu/bs.git master
-    git subtree pull --prefix $path/basic git@github.com:rexbu/basic.git master
-    git subtree pull --prefix $path/3rdparty/libzip git@github.com:rexbu/libzip.git  master
-    git subtree pull --prefix $path/model git@github.com:rexbu/model.git master
+    git subtree pull --prefix ${path}bs git@github.com:rexbu/bs.git master
+    git subtree pull --prefix ${path}basic git@github.com:rexbu/basic.git master
+    git subtree pull --prefix ${path}3rdparty/libzip git@github.com:rexbu/libzip.git  master
+    git subtree pull --prefix ${path}model git@github.com:rexbu/model.git master
 else
-    echo 'please use ./setup.sh {path} {action}. action:init|push|pull. eg: ./setup.sh ./ push'
+    echo "please use ./setup.sh {action} {path}. action:init|push|pull. when path is current dir, don't use path. eg: ./setup.sh push"
 fi

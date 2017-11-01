470a3ee4d51ea249aae59920e6232faecde5eeee
rexbu <rex@labjk.com>
2017-11-01 17:59:02 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-git remote add bs git@github.com:rexbu/bs.git
-git remote add basic git@github.com:rexbu/basic.git
-git remote add libzip git@github.com:rexbu/libzip.git
-git remote add model git@github.com:rexbu/model.git
+action=$1
+path=$2
 
-git subtree add --prefix bs bs master
-git subtree add --prefix basic basic master
-git subtree add --prefix model model master
-git subtree add --prefix 3rdparty/libzip libzip master
+if [ "$action" = "init" ];then
+    git remote add $path/bs git@github.com:rexbu/bs.git
+    git remote add $path/basic git@github.com:rexbu/basic.git
+    git remote add $path/libzip git@github.com:rexbu/libzip.git
+    git remote add $path/model git@github.com:rexbu/model.git
+
+    git subtree add --prefix $path/bs bs master
+    git subtree add --prefix $path/basic basic master
+    git subtree add --prefix $path/model model master
+    git subtree add --prefix $path/3rdparty/libzip libzip master
+elif [ "$action" = "push" ];then
+    git subtree push --prefix $path/bs git@github.com:rexbu/bs.git master
+    git subtree push --prefix $path/basic git@github.com:rexbu/basic.git master
+    git subtree push --prefix $path/3rdparty/libzip git@github.com:rexbu/libzip.git  master
+    git subtree push --prefix $path/model git@github.com:rexbu/model.git master
+elif [ "$action" = "pull" ];then
+    git subtree pull --prefix $path/bs git@github.com:rexbu/bs.git master
+    git subtree pull --prefix $path/basic git@github.com:rexbu/basic.git master
+    git subtree pull --prefix $path/3rdparty/libzip git@github.com:rexbu/libzip.git  master
+    git subtree pull --prefix $path/model git@github.com:rexbu/model.git master
+else
+    echo 'please use ./setup.sh {path} {action}. action:init|push|pull. eg: ./setup.sh ./ push'
+fi

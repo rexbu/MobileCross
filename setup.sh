11f831abc3c8916f806b7ef73c90ae86c1e1b9e1
rexbu <rex@labjk.com>
2017-11-01 19:54:49 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -2,28 +2,40 @@
 action=$1
 path=$2
 
-echo "path:"$path
+
+modules=("bs" "basic" "libzip" "model")
+remotes=("git@github.com:rexbu/bs.git" "git@github.com:rexbu/basic.git" "git@github.com:rexbu/libzip.git" "git@github.com:rexbu/model.git")
+localdirs=("bs" "basic" "3rdparty/libzip" "model")
+
+echo "---- start: ${BASH_SOURCE}"
 
 if [ "$action" = "init" ];then
-    git remote add bs git@github.com:rexbu/bs.git
-    git remote add basic git@github.com:rexbu/basic.git
-    git remote add libzip git@github.com:rexbu/libzip.git
-    git remote add model git@github.com:rexbu/model.git
-
-    git subtree add --prefix ${path}bs bs master
-    git subtree add --prefix ${path}basic basic master
-    git subtree add --prefix ${path}model model master
-    git subtree add --prefix ${path}3rdparty/libzip libzip master
+    for ((i=0; i<${#modules[@]};i++)); do
+        git remote add ${modules[i]} ${remotes[i]}
+        git subtree add --prefix ${path}${localdirs[i]} ${modules[i]} master
+        setup=${path}${localdirs[i]}/setup.sh
+        if [ -f "$setup" ];then
+            $setup $action ${path}${localdirs[i]}/ 
+        fi
+    done
 elif [ "$action" = "push" ];then
-    git subtree push --prefix ${path}bs git@github.com:rexbu/bs.git master
-    git subtree push --prefix ${path}basic git@github.com:rexbu/basic.git master
-    git subtree push --prefix ${path}3rdparty/libzip git@github.com:rexbu/libzip.git  master
-    git subtree push --prefix ${path}model git@github.com:rexbu/model.git master
+    for ((i=0; i<${#modules[@]};i++)); do
+        setup=${path}${localdirs[i]}/setup.sh
+        if [ -f "$setup" ];then
+            $setup $action ${path}${localdirs[i]}/ 
+        fi
+        git subtree push --prefix ${path}${localdirs[i]} ${remotes[i]} master
+    done
 elif [ "$action" = "pull" ];then
-    git subtree pull --prefix ${path}bs git@github.com:rexbu/bs.git master
-    git subtree pull --prefix ${path}basic git@github.com:rexbu/basic.git master
-    git subtree pull --prefix ${path}3rdparty/libzip git@github.com:rexbu/libzip.git  master
-    git subtree pull --prefix ${path}model git@github.com:rexbu/model.git master
+    for ((i=0; i<${#modules[@]};i++)); do
+        setup=${path}${localdirs[i]}/setup.sh
+        if [ -f "$setup" ];then
+            $setup $action ${path}${localdirs[i]}/ 
+        fi
+        git subtree pull --prefix ${path}${localdirs[i]} ${remotes[i]} master
+    done
 else
     echo "please use ./setup.sh {action} {path}. action:init|push|pull. when path is current dir, don't use path. eg: ./setup.sh push"
 fi
+
+echo "#### end: ${BASH_SOURCE}"
